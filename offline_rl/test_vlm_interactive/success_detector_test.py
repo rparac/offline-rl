56d2d82b6094d99590b9235721a6b46b8cb17551
Roko Parac <roko.parac@gmail.com>
2025-12-09 16:59:15 +0000

    Fix path in successor test and use in-context example

--- offline_rl/test_vlm_interactive/success_detector_test.py
+++ offline_rl/test_vlm_interactive/success_detector_test.py
@@ -16,20 +16,28 @@ import torch
 
 from offline_rl.vlm.success_detector import compute_similarities, load_similarity_model
 
-
-goal_prompt = "robot is on top of a pickaxe"
+goal_prompt = "A man in a grey sweater and blue trousers, captured mid-swing while playing golf"
 negative_prompts = [
-    "robot is on top of an empty cell",
-    "robot is on top of a gem",
-    "robot is on top of a door",
-    "robot is on top of a lava",
+    "A dog playing with a stick",
+    "A red racing car",
+    "A blue bicycle",
 ]
+
+
+# goal_prompt = "robot is on top of a pickaxe"
+# negative_prompts = [
+#     "robot is on top of an empty cell",
+#     "robot is on top of a gem",
+#     "robot is on top of a door",
+#     "robot is on top of a lava",
+# ]
 
 batch_size = 1
 num_workers = 1
 
 
 image = Image.open("debug_env_images/episode_0_step_0.png")
+image = Image.open("debug_env_images/in_context_image.png")
 # Convert PIL image to numpy array, then to tensor
 # Format: (H, W, C) -> (1, H, W, C) with batch dimension
 image_array = np.array(image)
@@ -37,11 +45,13 @@ image_array = np.array(image)
 frames = torch.from_numpy(image_array).unsqueeze(0).to(torch.uint8)
 
 
+model_name = "ViT-bigG-14/laion2b_s39b_b160k"
+
 similarity_model = load_similarity_model(
-    model_name="ViT-L-14/laion2b_s32b_b82k",
+    model_name=model_name,
     goal_prompt=goal_prompt,
     negative_prompts=negative_prompts,
-    cache_dir="vlm/cache",
+    cache_dir="/data/private/rp218/open_clip",
 )
 
 
